8a2efdd5603ba33ffbe37641cfe5029056b9a0cf
mowsong <goku999@sina.com>
2018-07-04 13:27:35 +0800

    stupid tabs

--- Work/hello/main.c
+++ Work/hello/main.c
@@ -75,17 +75,17 @@
 #include "task.h"
 
 /* Priorities at which the tasks are created. */
-#define mainCHECK_TASK_PRIORITY			( configMAX_PRIORITIES - 2 )
-#define mainQUEUE_POLL_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define mainSEM_TEST_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define mainBLOCK_Q_PRIORITY			( tskIDLE_PRIORITY + 2 )
-#define mainCREATOR_TASK_PRIORITY		( tskIDLE_PRIORITY + 3 )
-#define mainINTEGER_TASK_PRIORITY		( tskIDLE_PRIORITY )
-#define mainGEN_QUEUE_TASK_PRIORITY		( tskIDLE_PRIORITY )
-#define mainFLOP_TASK_PRIORITY			( tskIDLE_PRIORITY )
-#define mainQUEUE_OVERWRITE_PRIORITY	( tskIDLE_PRIORITY )
+#define mainCHECK_TASK_PRIORITY         ( configMAX_PRIORITIES - 2 )
+#define mainQUEUE_POLL_PRIORITY         ( tskIDLE_PRIORITY + 1 )
+#define mainSEM_TEST_PRIORITY           ( tskIDLE_PRIORITY + 1 )
+#define mainBLOCK_Q_PRIORITY            ( tskIDLE_PRIORITY + 2 )
+#define mainCREATOR_TASK_PRIORITY       ( tskIDLE_PRIORITY + 3 )
+#define mainINTEGER_TASK_PRIORITY       ( tskIDLE_PRIORITY )
+#define mainGEN_QUEUE_TASK_PRIORITY     ( tskIDLE_PRIORITY )
+#define mainFLOP_TASK_PRIORITY          ( tskIDLE_PRIORITY )
+#define mainQUEUE_OVERWRITE_PRIORITY    ( tskIDLE_PRIORITY )
 
-#define mainTIMER_TEST_PERIOD			( 50 )
+#define mainTIMER_TEST_PERIOD           ( 50 )
 
 
 /*
@@ -100,11 +100,11 @@ void vApplicationTickHook( void );
 
 static void prvHelloTask ( void *pvParameters )
 {
-	( void ) pvParameters;
+    ( void ) pvParameters;
 
     while (1) {
         printf("Hello world - %u\n", ( unsigned int ) xTaskGetTickCount() );
-		fflush( stdout );
+        fflush( stdout );
         vTaskDelay(1000);
     }
 }
@@ -115,57 +115,57 @@ int main ( void )
 {
     xTaskCreate( prvHelloTask, "Hello", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
 
-	/* Start the scheduler itself. */
-	vTaskStartScheduler();
+    /* Start the scheduler itself. */
+    vTaskStartScheduler();
 
-	/* Should never get here unless there was not enough heap space to create
-	the idle and other system tasks. */
-	return 0;
+    /* Should never get here unless there was not enough heap space to create
+    the idle and other system tasks. */
+    return 0;
 }
 
 /*-----------------------------------------------------------*/
 
 void vApplicationMallocFailedHook( void )
 {
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
-	vAssertCalled( __LINE__, __FILE__ );
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+    vAssertCalled( __LINE__, __FILE__ );
 }
 /*-----------------------------------------------------------*/
 
 void vApplicationIdleHook( void )
 {
-	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-	task.  It is essential that code added to this hook function never attempts
-	to block in any way (for example, call xQueueReceive() with a block time
-	specified, or call vTaskDelay()).  If the application makes use of the
-	vTaskDelete() API function (as this demo application does) then it is also
-	important that vApplicationIdleHook() is permitted to return to its calling
-	function, because it is the responsibility of the idle task to clean up
-	memory allocated by the kernel to any task that has since been deleted. */
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    task.  It is essential that code added to this hook function never attempts
+    to block in any way (for example, call xQueueReceive() with a block time
+    specified, or call vTaskDelay()).  If the application makes use of the
+    vTaskDelete() API function (as this demo application does) then it is also
+    important that vApplicationIdleHook() is permitted to return to its calling
+    function, because it is the responsibility of the idle task to clean up
+    memory allocated by the kernel to any task that has since been deleted. */
 
-		/* Call the idle task processing used by the full demo.  The simple
-		blinky demo does not use the idle task hook. */
-		//vFullDemoIdleFunction();
+        /* Call the idle task processing used by the full demo.  The simple
+        blinky demo does not use the idle task hook. */
+        //vFullDemoIdleFunction();
 }
 /*-----------------------------------------------------------*/
 
 void vApplicationTickHook( void )
 {
-	/* This function will be called by each tick interrupt if
-	configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
-	added here, but the tick hook is called from an interrupt context, so
-	code must not attempt to block, and only the interrupt safe FreeRTOS API
-	functions can be used (those that end in FromISR()). */
+    /* This function will be called by each tick interrupt if
+    configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+    added here, but the tick hook is called from an interrupt context, so
+    code must not attempt to block, and only the interrupt safe FreeRTOS API
+    functions can be used (those that end in FromISR()). */
 
 }
 
@@ -173,12 +173,12 @@ void vApplicationTickHook( void )
 
 void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
 {
- 	taskENTER_CRITICAL();
-	{
+    taskENTER_CRITICAL();
+    {
         printf("[ASSERT] %s:%lu\n", pcFileName, ulLine);
         fflush(stdout);
-	}
-	taskEXIT_CRITICAL();
-	exit(-1);
+    }
+    taskEXIT_CRITICAL();
+    exit(-1);
 }
 /*-----------------------------------------------------------*/
